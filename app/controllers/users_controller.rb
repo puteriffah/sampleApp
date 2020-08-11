d7353970cf96216c65b26715df757bbffaf55079
puteriffah <puteri153@gmail.com>
2020-08-11 17:19:23 +0800

    init project

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      flash[:danger] = "Danger! please try again"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit User::USERS_PARAMS
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root "static_pages#home"
 
   get "static_pages/help"
+  get "users/show"
+  get "users/new"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users
 end
-
